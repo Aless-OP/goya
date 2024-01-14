952b67ad6e438e7ffde5fdab0ceac2e369c83235
Aless <103533672+Aless-OP@users.noreply.github.com>
2024-01-14 12:32:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu-l').classList.toggle('show');
+ });
+ 
+ ScrollReveal().reveal('.nav-main', { delay: 250 });
+ ScrollReveal().reveal('.hr', { delay: 250 });
+ ScrollReveal().reveal('.showcase', { delay: 250 });
+ ScrollReveal().reveal('.showcase2', { delay: 250 });
+ ScrollReveal().reveal('.showcase3', { delay: 250 });
+ ScrollReveal().reveal('.showcase4', { delay: 250 });
+ ScrollReveal().reveal('.paintings-title', { delay: 250 });
+ ScrollReveal().reveal('.featured-paintings', { delay: 250 });
+ ScrollReveal().reveal('.banner', { delay: 250 });
+ ScrollReveal().reveal('.cuadroh1', { delay: 250 });
+ ScrollReveal().reveal('.info', { delay: 250 });
